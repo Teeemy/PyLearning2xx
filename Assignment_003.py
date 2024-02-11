9c61d5d91a1a31b508efa5aafe369ebbc730b606
My Name <onibonojemariam@gmail.com>
2024-02-11 20:42:28 +0100

    hi

--- Assignment_003.py
+++ Assignment_003.py
@@ -1,5 +1,9 @@
+# write a program to calculate the area of a circle
+# given its radius using the formula area= πxr^2 (Take pie as 3.14)
+
 import math
-radius = float (input("Enter the radius"))
-print(math.pi)
+radius = float (input("Enter the radius"))# take input from user
+print(3.14*radius**2)
+#print(math.pi)
 area = math.pi*pow(radius, 2)
-print(area)
+#print(area)
